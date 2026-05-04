18fd75617c55dfa575366e521192a1644a9cd3ca
Visivel <ymikacraft@gmail.com>
2026-05-04 00:47:57 -0300

    nova path

--- src/config/yamlManager.ts
+++ src/config/yamlManager.ts
@@ -6,7 +6,7 @@ interface Config{
     senha: string
 }
 
-const arquivo = fs.readFileSync('./src/config.yaml', 'utf-8')
+const arquivo = fs.readFileSync('./config.yaml', 'utf-8')
 const lidos = parse(arquivo)
 
 export const dados: Config = lidos.bot
